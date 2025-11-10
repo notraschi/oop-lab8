bf3a4f94f02d8edb132188fee1d47f53a196db29
notraschi <nomeutentedisponigfr@gmail.com>
2025-11-10 15:28:59 +0100

    81: done

--- 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -56,7 +56,7 @@ public class MiniGUI {
         write.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent e) {
-                System.out.println(randomGenerator.nextInt());
+                result.setText(randomGenerator.nextInt()+"");
             }
         });
     }
