88d9868bc4de40915c2b9bbdc5503beb016f3392
notraschi <nomeutentedisponigfr@gmail.com>
2025-11-11 03:32:49 +0100

    83: i think i implemented the stdout view

---
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberStandardOutputView.java
@@ -0,0 +1,23 @@
+package it.unibo.mvc.view;
+
+import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawResult;
+
+public class DrawNumberStandardOutputView implements DrawNumberView {
+
+    @Override
+    public void setController(DrawNumberController observer) {
+        throw new UnsupportedOperationException("this view is output only");
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void result(DrawResult res) {
+        System.out.println(res);
+    }
+    
+}
